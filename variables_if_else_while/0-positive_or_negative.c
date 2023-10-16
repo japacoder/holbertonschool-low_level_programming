7dd8fffe4adb590ca3d956bbebba25daee1882c9
japacoder <deg.mahy@gmail.com>
2023-10-16 08:09:00 -0700

    debut du code c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
+/**
  * main - this program will assign a random num the varia
  * Return: 0 is succes
  */
@@ -17,9 +17,9 @@ int main(void)
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);	
+	printf("%d is positive\n", n);
 	}
-        else if (n == 0)
+	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
