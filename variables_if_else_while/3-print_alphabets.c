a5c776ddcbaab183e63ef9c4e4a7182bdcfe5531
japacoder <deg.mahy@gmail.com>
2023-10-19 15:58:20 -0700

    you can only use the putchar prints the alphabet in lowercas

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-print _alphabets.c
+ */
+
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase, and then in uppercase.
+*
+ Return: always 0.
+*/
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
+
